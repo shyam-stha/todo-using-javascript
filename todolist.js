b0749b0b91d191f7ebd427130391066af2d82612
Runal Manandhar <runalmanandhar@gmail.com>
2022-09-21 18:27:43 +0545

    Features: add todo list
    Features: display todo list with console
    
    initialize

--- todolist.js
+++ todolist.js
@@ -1,19 +1,38 @@
 const task= [];
 const addbtn = document.getElementById('addbtn');
 const removebtn = document.getElementById('removebtn');
-var display=document.querySelector('.display')
+var display=document.querySelector('.display');
+var consoleDislplay=document.querySelector('.display-console');
+var userInput;
 addbtn.addEventListener("click",(e)=>{
-    
-    const input =document.getElementById('input').value;
+    userInput=document.getElementById('input');
+    var input =userInput.value;
     task.push(input);
-    task.forEach((val)=>{
-        let value= val;
-        display.innerHTML= value;
-        console.log(value)
-    });
+    display.innerHTML='';
+    displayTodoList();
+});
+
+consoleDislplay.addEventListener('click',e=>{
+    console.log('Final Todo list',task);
 })
-        
-       
+
+const displayTodoList=()=>{
+    task.forEach(val=>{
+        display.innerHTML+=`
+        <div class='todo-list'>
+            <input class='todo-list__input' value=' ${val}' disabled/>
+            <div class='todo-list__btn'>
+                <button class='todo-list__btn--edit' id='edit' >
+                    Edit
+                </button>
+                <button class='todo-list__btn--delete'id='delete'>
+                    Delete
+                </button>
+            </div>
+        </div>
+         ` ;
+         userInput.value='';
+    });
+}
         
    
-
