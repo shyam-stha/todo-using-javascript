2c5d51c5feea4113280383092e073487a5956a2c
Shyam Stha <sthask2019@gmail.com>
2022-12-20 18:01:42 +0545

    fix : Add different funtionality and fix some bugs

--- todolist.js
+++ todolist.js
@@ -1,68 +1,81 @@
-const task= [];
-const addbtn = document.getElementById('addbtn');
-const removebtn = document.getElementById('removebtn');
-var display=document.querySelector('.display');
-var consoleDislplay=document.querySelector('.display-console');
+const task = [];
+const addbtn = document.getElementById("addbtn");
+const removeall = document.getElementById("removebtn");
+var display = document.querySelector(".display");
+var consoleDislplay = document.querySelector(".display-console");
 var userInput;
-// const input = getElementById('input');
-// input.addEventListener('keyup',(e)=>{
-//     let value= e.currentTarget.value;
-//     addbtn.disabled = false;
-//     if(value===""){
-//         addbtn.disabled= true;
-//     }
-// })
+var todolists;
 
-addbtn.addEventListener("click",(e)=>{
-    userInput=document.getElementById('input');
-    var input =userInput.value;
+addbtn.addEventListener("click", (e) => {
+  userInput = document.getElementById("input");
+  var input = userInput.value;
+  if (input === "") {
+    alert("Please Add Task,...");
+  } else {
     task.push(input);
-    display.innerHTML='';
+    display.innerHTML = "";
     displayTodoList();
+  }
 });
 
-consoleDislplay.addEventListener('click',e=>{
-    console.log('Final Todo list',task);
-})
+consoleDislplay.addEventListener("click", (e) => {
+  console.log("Final Todo list", task);
+});
 
-const displayTodoList=()=>{
-    task.forEach(val=>{
-        display.innerHTML+=`
-        <div class='todo-list'id='main'>
-            <input class='todo-list__input' value=' ${val}' disabled/ id='input'>
+const displayTodoList = () => {
+  task.forEach((val) => {
+    display.innerHTML += `
+        <div class='todo-list'>
+            <input class='todo-list__input' value=' ${val}' id='todo__list--edit-input' disabled/>
             <div class='todo-list__btn'>
-                <button class='todo-list__btn--edit' id='edit' >
+                <button class='todo-list__btn--edit' >
                     Edit
                 </button>
-                <button class='todo-list__btn--delete'id='delete'>
+                <button class='todo-list__btn--delete'>
                     Delete
                 </button>
+                <button class='todo-list__btn--save' hidden>
+                    Save
+                </button>
+                
             </div>
         </div>
-         ` ;
-         userInput.value='';
+         `;
+    userInput.value = "";
+  });
+  getTodoList();
+};
+
+const getTodoList = () => {
+  todolists = document.querySelectorAll(".todo-list");
+  console.log("todol list all here", todolists);
+  todolists.forEach((list, key) => {
+    console.log("lists here", key);
+    let editBtn = list.children[1].children[0];
+    let delteBtn = list.children[1].children[1];
+    let savebtn = list.children[1].children[2];
+
+    editBtn.addEventListener("click", (e) => {
+      savebtn.hidden = false;
+      editinput = list.children[0];
+      editinput.disabled = false;
+      document.getElementById("todo__list--edit-input").focus();
     });
 
-}
-    //   var editbtn= document.getElementById('edit');
-    //   editbtn.addEventListener('click',()=>{
-    //     console.log('clicked');
-    //   })
-    //     const main = document.getElementById('main');
-    //       var input= document.getElementById('input');
-    //       console.log('hi',input)
-    //       main.forEach(val=>{
-    //           input.disabled = false;
-            
-    //       })
-    //     })
-  
-  //    const deletebtn= document.getElementById('delete');
-  //    deletebtn.addEventListener('click', e=>{
-  //    task.map(function(element){
-  //     task.pop();
-  //    })
-  // })
-          
-        
-
+    delteBtn.addEventListener("click", (e) => {
+      task.splice(key, 1);
+      console.log(task);
+      display.innerHTML = "";
+      displayTodoList();
+    });
+    savebtn.addEventListener("click", (e) => {
+      editinput.disabled = true;
+      task.splice(key, 1, editinput.value);
+      savebtn.hidden = true;
+    });
+  });
+};
+removeall.addEventListener("click", (e) => {
+  task.length = 0;
+  display.innerHTML = "";
+});
